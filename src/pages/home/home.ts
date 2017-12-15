0ca9d1a15cdfd1748804def83eb0fa7424a5eb70
makni <makni.firas.ing@gmail.com>
2017-12-15 11:24:58 +0100

    geo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, NgModule} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Geolocation} from '@ionic-native/geolocation';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
+})
+@NgModule({
+    providers: [
+        Geolocation
+    ]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, private geolocation: Geolocation) {
 
-  }
+    }
 
+
+    search() {
+        alert("ok");
+        const subscription = this.geolocation.watchPosition()
+        //   .filter((p) => p.coords !== undefined) //Filter Out Errors
+            .subscribe(position => {
+                alert(position.coords.longitude + ' ' + position.coords.latitude);
+            });
+
+// To stop notifications
+        subscription.unsubscribe();
+    }
+
+    stop() {
+
+    }
 }
